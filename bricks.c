e33c451df706e642ec46d6d5985144d08b8e049b
mandoju-umaa <mandojuuma3@gmail.com>
2026-02-03 15:40:04 +0530

    Add files via upload

---
+++ bricks.c
@@ -0,0 +1,39 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void main()
+{
+    int small;
+    int big;
+    int goal;
+    printf("enter the no,of small bricks\n");
+    scanf("%d",&small);
+    printf("enter the no,of big bricks\n");
+    scanf("%d",&big);
+    printf("enter the no,of reruired \n");
+    scanf("%d",&goal);
+    int required=goal/5;
+    if(required<=big)
+    {
+        int rem=goal-(required*5);
+        if(rem<=small)
+        {
+            printf("true");
+        }
+        else
+        {
+            printf("false");
+        }
+    }
+        else
+        {
+            int rem=goal-(big*5);
+            if (rem<=small)
+            {
+                printf("true");
+            }
+            else
+            {
+                printf("false");
+            }
+        }
+}
